108850b1d291de4e02fc4ff27c24f4e39d05f31a
LoggingCo <ksy9527s@gamil.com>
2022-08-22 19:46:30 +0900

    close btn#1

--- src/pages/mainPage/index.tsx
+++ src/pages/mainPage/index.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import LoginForm from 'components/mainCp/loginForm';
 import MainBanner from 'components/mainCp/mainBanner';
 import MainLayout from 'layout/mainLayout';
 import { MainPageInner } from './style';
 import SignModal from 'components/mainCp/signModal';
+import { useNavigate } from 'react-router-dom';
 
 const MainPage = () => {
     const [modal, setModal] = useState<boolean>(false);
+
+    const navigate = useNavigate();
+    const auth = useRef<string | null>(
+        localStorage.getItem(process.env.REACT_APP_TOEKN_KEY as string),
+    );
+
+    useEffect(() => {
+        if (auth.current) {
+            navigate('/todo', { replace: true });
+        } else {
+            return;
+        }
+    }, [navigate]);
 
     return (
         <>
